fd134a8ca12bf37daf8bd68a31ae9ffe6f163d4a
Dixon Gunasekara <dixon@randomsoftware.net>
2020-05-05 00:22:17 +0530

    php template

--- contact.php
+++ contact.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="css/styles.css" />
-  </head>
-  <body>
-    <div class="header">
-      <ul>
-        <li><a href="home.php">Home</a></li>
-        <li><a href="news.php">News</a></li>
-        <li><a class="active" href="contact.php">Contact</a></li>
-        <li><a href="about.php">About</a></li>
-      </ul>
-    </div>
-    <div class="main-content">Contact - Content</div>
-    <div class="footer">Footer</div>
-  </body>
+
+<head>
+  <title>Home</title>
+  <link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+
+<body>
+  <?php require "header.php" ?>
+  <div class="main-content">Contact - Content</div>
+  <?php require "footer.php" ?>
+</body>
+
 </html>
---
+++ header.php
@@ -0,0 +1,8 @@
+<div class="header">
+    <ul>
+        <li><a class="active" href="home.php">Home</a></li>
+        <li><a href="news.php">News</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="about.php">About</a></li>
+    </ul>
+</div>
