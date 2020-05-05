b8f10bc6d7396cedc2f9fd0d2ad8b928e53f5b88
Dixon Gunasekara <dixon@randomsoftware.net>
2020-05-05 08:32:49 +0530

    php template

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <div class="header">
     <ul>
-        <li><a class="<?php $active == 'home' ? 'active' : '' ?>" href="home.php">Home</a></li>
-        <li><a class="<?php $active == 'news' ? 'active' : '' ?>" href="news.php">News</a></li>
-        <li><a class="<?php $active == 'contact' ? 'active' : '' ?>" href="contact.php">Contact</a></li>
-        <li><a class="<?php $active == 'about' ? 'active' : '' ?>" href="about.php">About</a></li>
+        <li><a class="<?php $active == 'home' ? echo 'active'; : echo ''; ?>" href="home.php">Home</a></li>
+        <li><a class="<?php $active == 'news' ? echo 'active' : echo ''; ?>" href="news.php">News</a></li>
+        <li><a class="<?php $active == 'contact' ? echo 'active' : echo ''; ?>" href="contact.php">Contact</a></li>
+        <li><a class="<?php $active == 'about' ? echo 'active' : echo ''; ?>" href="about.php">About</a></li>
     </ul>
 </div>
