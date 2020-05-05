ecc060df506703f172f568678f83755b287d5649
Dixon Gunasekara <dixon@randomsoftware.net>
2020-05-05 11:26:25 +0530

    php mysql

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="css/styles.css" />
-    <link rel="stylesheet" type="text/css" href="css/index.css" />
-  </head>
-  <body>
-    <div id="login-div">
-      <form id="login-form" action="home.php">
-        <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  </body>
+
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="css/styles.css" />
+  <link rel="stylesheet" type="text/css" href="css/index.css" />
+</head>
+
+<body>
+  <div id="login-div">
+    <form id="login-form" action="process_login.php" method="POST">
+      <input type="text" placeholder="email" />
+      <input type="password" placeholder="password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</body>
+
 </html>
---
+++ process_login.php
@@ -0,0 +1,3 @@
+<?php
+require "functions.php";
+header("home.php");
